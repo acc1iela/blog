e1b9effbdecd7619af5c9a9bebdc996af64b3fae
acc1iela <acc1iela@gmail.com>
2023-07-09 03:11:37 +0900

    update: next themeインストール Providers.tsx作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './components/Navbar';
+import { Providers } from './components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={` ${inter.className} bg-white text-black dark:bg-gray-900 dark:selection::bg-gray-900 dark:text-white h-full selection:bg-gray`}
       >
-        <Navbar />
-        {children}
+        <Providers>
+          <Navbar />
+          <main className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
+        </Providers>
       </body>
     </html>
   );
