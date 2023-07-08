436c86bf900353ead6b724bf6eabc642b62409fa
acc1iela <acc1iela@gmail.com>
2023-07-09 03:12:46 +0900

    update: ハイドレーションエラーを抑制

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <body
         className={` ${inter.className} bg-white text-black dark:bg-gray-900 dark:selection::bg-gray-900 dark:text-white h-full selection:bg-gray`}
       >
