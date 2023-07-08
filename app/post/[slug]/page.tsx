288196829f502d72a70d57b3bcb2ab086a2b9383
acc1iela <acc1iela@gmail.com>
2023-07-09 04:44:08 +0900

    update: portabletext install

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { client } from '@/app/lib/sanity';
+import { PortableText } from '@portabletext/react';
 
 async function getData(slug: string) {
   const query = `*[_type == 'post' && slug.current == '${slug}'][0]`;
@@ -11,7 +12,7 @@ export default async function SlugPage({ params }: { params: { slug: string } })
   const data = await getData(params.slug);
 
   return (
-    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+    <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
       <header className="pt-6 xl:pb-6">
         <div className="space-y-1 text-center">
           <div className="space-y-10">
@@ -21,8 +22,21 @@ export default async function SlugPage({ params }: { params: { slug: string } })
               </p>
             </div>
           </div>
+          <div>
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+              {data.title}
+            </h1>
+          </div>
         </div>
       </header>
+
+      <div className="divide-y divide-gray-200 pb-7 dark:divide-gray-700 xl:divide-y-0">
+        <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+          <div className="prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
+            <PortableText value={data.content} components={{}} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
